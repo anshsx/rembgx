c06eb2b24311f0dd8fd7973de97ac6ea4c806956
anshsx <erasearch.co@gmail.com>
2025-02-05 16:51:42 +0530

    Create main.py

---
+++ app/main.py
@@ -0,0 +1,30 @@
+from fastapi import FastAPI, UploadFile, File
+from app.remove_bg import remove_background
+from fastapi.responses import FileResponse
+import shutil
+import os
+import uuid
+
+app = FastAPI()
+
+UPLOAD_DIR = "uploads"
+RESULTS_DIR = "results"
+os.makedirs(UPLOAD_DIR, exist_ok=True)
+os.makedirs(RESULTS_DIR, exist_ok=True)
+
+@app.post("/remove-bg/")
+async def remove_bg(file: UploadFile = File(...)):
+    file_id = str(uuid.uuid4())
+    input_path = f"{UPLOAD_DIR}/{file_id}.png"
+    output_path = f"{RESULTS_DIR}/{file_id}.png"
+    
+    with open(input_path, "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+
+    remove_background(input_path, output_path)
+    
+    return FileResponse(output_path, media_type="image/png")
+
+@app.get("/")
+def home():
+    return {"message": "Background Remover API is running!"}
